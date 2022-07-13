7f2a1078ecde46b87bf272ced3bbeba3115ef53d
santosh-ksh7 <santoshksh7@gmail.com>
2022-07-13 20:10:29 +0530

    changed from id to _id to uniquely identify

--- routes/helper.js
+++ routes/helper.js
@@ -1,16 +1,17 @@
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export async function createMovies(data) {
     return await client.db("guvi").collection("movies").insertOne(data);
 }
 export async function updateMoviesById(id, data) {
-    return await client.db("guvi").collection("movies").updateOne({ id: id }, { $set: data });
+    return await client.db("guvi").collection("movies").updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMoviesById(id) {
-    return await client.db("guvi").collection("movies").deleteOne({ id: id });
+    return await client.db("guvi").collection("movies").deleteOne({ _id: ObjectId(id) });
 }
 export async function getMoviesById(id) {
-    return await client.db("guvi").collection("movies").findOne({ id: id });
+    return await client.db("guvi").collection("movies").findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies(request) {
     return await client.db("guvi").collection("movies").find(request.query).toArray();
